1b7af6c6f702f2e1338b526f0da72bfb032f807d
c5244525 <bismay.kumar.biswal@sap.com>
2019-03-03 18:27:35 +0530

    fixed

--- src/main/java/com/bismay/app/finance/controller/PasswordResetController.java
+++ src/main/java/com/bismay/app/finance/controller/PasswordResetController.java
@@ -3,6 +3,7 @@ package com.bismay.app.finance.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindException;
@@ -24,6 +25,7 @@ public class PasswordResetController {
 	
 	@Autowired private UserService userService;
     @Autowired private PasswordResetTokenRepository tokenRepository;
+    @Lazy
     @Autowired private BCryptPasswordEncoder passwordEncoder;
     
     @GetMapping("/reset-password")
