57a69b5c608e1c5659f2b7fdf6699e4831e32eaf
c5244525 <bismay.kumar.biswal@sap.com>
2019-03-03 18:21:24 +0530

    fixed

--- src/main/java/com/bismay/app/finance/config/ResourceServerConfig.java
+++ src/main/java/com/bismay/app/finance/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.bismay.app.finance.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -12,13 +13,15 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    @Autowired
+    
+	@Autowired
     private ResourceServerTokenServices tokenServices;
 
     @Value("${security.jwt.resource-ids}")
     private String resourceIds;
 
     @Override
+    @Primary
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId(resourceIds).tokenServices(tokenServices);
     }
